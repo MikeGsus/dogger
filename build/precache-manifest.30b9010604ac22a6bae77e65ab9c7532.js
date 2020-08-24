c75d9b9f533e4f590846716a1da1b29607ed0a35
MikeGsus <mike08ahj@hotmail.com>
2020-08-24 18:19:15 -0500

    Add build

---
+++ build/precache-manifest.30b9010604ac22a6bae77e65ab9c7532.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da6614739f249afafaa6160df1619cf4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2948ae11fb90fbb4499",
+    "url": "/static/css/main.5e611659.chunk.css"
+  },
+  {
+    "revision": "41bef27c237ab4cbb0f4",
+    "url": "/static/js/2.c99ad9ed.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/static/js/2.c99ad9ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2948ae11fb90fbb4499",
+    "url": "/static/js/main.c5c25a43.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "7b3bf80900bcecbf8c4837d123f1d17c",
+    "url": "/static/media/dogger_icon.7b3bf809.png"
+  },
+  {
+    "revision": "16320d16dcf4dc1ff2bd343a1d86e24c",
+    "url": "/static/media/dogger_logo.16320d16.png"
+  }
+]);
