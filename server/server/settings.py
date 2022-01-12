b03e5bcc51651203adc95549d6cf9fcb5fc9dcee
Luis Juarez <luismanueljf@gmail.com>
2022-01-12 11:36:14 -0600

    add db connection

--- server/server/settings.py
+++ server/server/settings.py
@@ -83,11 +83,16 @@ WSGI_APPLICATION = 'server.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "dbpvs7oqp9e57i",
+        "USER": "aplqsgbmnksdgm",
+        "PASSWORD": "b03bcab8f4ed89447baa0fdfb6c0eea2df43787c858687b444c29b25450e6e87",
+        "HOST": "ec2-3-227-15-75.compute-1.amazonaws.com",
+        "PORT": "5432",
     }
 }
+
 
 
 # Password validation
